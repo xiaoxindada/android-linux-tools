c419ddf029fc63fb87e83571d41b290e14afff02
xiaoxindada <2245062854@qq.com>
2022-07-09 01:44:51 +0800

    img2sdat: Fix bugs
    Signed-off-by: xiaoxindada <2245062854@qq.com>

--- img2sdat.sh
+++ img2sdat.sh
@@ -2,12 +2,14 @@
 
 # Copyright (C) 2020 Xiaoxindada <2245062854@qq.com>
 
-LOCALDIR=`cd "$( dirname ${BASH_SOURCE[0]} )" && pwd`
+LOCALDIR=$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)
 cd $LOCALDIR
 source ./bin.sh
+toolsdir="$LOCALDIR/img2sdat"
+dat_out_dir="$LOCALDIR/new_dats"
 
 function Usage() {
-cat <<EOT
+  cat <<EOT
 Usage:
 $0 <Image Path> [Other args]
   Image Path: Image Path
@@ -18,13 +20,13 @@ EOT
 }
 
 case $1 in
-  "-h"|"--help")
-    Usage
-    exit 1
-    ;;   
+"-h" | "--help")
+  Usage
+  exit 1
+  ;;
 esac
 
-if [ "$1" = "" ];then
+if [ "$1" = "" ]; then
   Usage
   exit 1
 fi
@@ -38,63 +40,67 @@ make_br="false"
 function img2simg() {
   rimg_file="$image"
   simg_file=$(echo "${image%%.*}" | sed 's/$/&s\.img/')
-  echo "Converting simg..."
+  echo "Converting to simg ..."
   $bin/img2simg "$rimg_file" "$simg_file"
-  if [ $? != "0" ];then
+  if [ $? != "0" ]; then
     echo "Convert failed!"
   else
     echo "Convert success"
-    mv -f $simg_file $bin/img2sdat/${image_name}.img
+    mv -f $simg_file $toolsdir/${image_name}.img
   fi
 }
 
 function simg2sdat() {
-  if [ ! -f $bin/img2sdat/${image_name}.img ];then
-    cp -frp $image $bin/img2sdat/${image_name}.img
+  if [ ! -f $toolsdir/${image_name}.img ]; then
+    cp -frp $image $toolsdir/${image_name}.img
   fi
-  cd $bin/img2sdat
-  rm -rf ./output
-  mkdir ./output
-  file ${image_name}.img
+  cd $toolsdir
+  rm -rf output
+  mkdir -p output
   echo "Create ${image_name}.new.dat..."
   python3 ./img2sdat.py "${image_name}.img" -o "output" -v "4" -p "$image_name"
-  if [ $? != "0" ];then
+  if [ $? != "0" ]; then
     echo "Convert failed!"
     rm -rf ${image_name}.img
+    cd $LOCALDIR
+    rm -rf $toolsdir/output
     exit 1
   else
     echo "${image_name}.new.dat create success"
     rm -rf ${image_name}.img
+    rm -rf $dat_out_dir
+    mkdir -p $dat_out_dir
+    mv output/* $dat_out_dir
     cd $LOCALDIR
-    rm -rf ./new_dat
-    mkdir ./new_dat
-    mv $bin/img2sdat/output/* ./new_dat/
-    echo "output: $LOCALDIR/new_dat"
+    rm -rf $toolsdir/output
+    echo "Output: $dat_out_dir"
   fi
 }
 
 function sdat2sdat_br() {
-  echo "Create ${image_name}.new.dat.br..."
-  $bin/brotli -q 0 $LOCALDIR/new_dat/${image_name}.new.dat -o $LOCALDIR/new_dat/${image_name}.new.dat.br
-  if [ $? != "0" ] ;then 
-    echo "${image_name}.new.dat.br Convert failed!"
-    exit
-   else
-    echo "${image_name}.new.dat.br create success"
-    echo "outpt: $LOCALDIR/new_dat/${image_name}.new.dat.br"
-  fi
+  while true; do
+    read -p "Need to create ${image_name}.new.dat.br? (y/n): " make_br
+    case $make_br in
+    "y")
+      echo "Create ${image_name}.new.dat.br..."
+      $bin/brotli -q 0 $dat_out_dir/${image_name}.new.dat -o $dat_out_dir/${image_name}.new.dat.br
+      if [ $? != "0" ]; then
+        echo "${image_name}.new.dat.br Convert failed!"
+        exit 1
+      else
+        echo "${image_name}.new.dat.br create success"
+        echo "outpt: $dat_out_dir/${image_name}.new.dat.br"
+      fi
+      break
+      ;;
+    "n") break ;;
+    *) echo "Invalid input!" ;;
+    esac
+  done
 }
 
-if ! (file $image | grep -qo "sparse") ;then
+if ! (file $image | grep -qo "sparse"); then
   img2simg
 fi
-
 simg2sdat
-
-if [ "$2" = "--make_br" ];then
-  make_br="true"
-fi
-
-if [ $make_br = "true" ];then
-  sdat2sdat_br
-fi
+sdat2sdat_br
